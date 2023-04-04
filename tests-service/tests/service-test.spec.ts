c67ec437e13380260d14a15de3ea400d8831d817
papatil <papatil@ncsu.edu>
2023-04-04 19:17:48 -0400

    Completed Service Tests

--- tests-service/tests/service-test.spec.ts
+++ tests-service/tests/service-test.spec.ts
@@ -7,7 +7,46 @@ describe("The server's Get Movies endpoint", () => {
   it("Checks that 651 movies are returned by the endpoint", (done) => {
     request.get(base_url + "movies", (error:Error | null, response:request.Response | null, body:string) => {
       expect(response?.statusCode).toBe(200);
-      expect(JSON.parse(body).length).toEqual(651);
+      const movies = JSON.parse(body);
+
+      const firstMovie = {
+        title: 'Filly Brown',
+        genre: 'Drama',
+        runtime: '80',
+        mpaa_rating: 'R',
+        release_year: '2013',
+        imdb_rating: '5.5',
+        critics_score: '45',
+        director: 'Michael D. Olmos',
+        actor1: 'Gina Rodriguez',
+        actor2: 'Jenni Rivera',
+        actor3: 'Lou Diamond Phillips',
+        actor4: 'Emilio Rivera',
+        actor5: 'Joseph Julian Soria'
+      };
+
+      const lastMovie = {
+      
+      "actor1": "Miley Cyrus",
+      "actor2": "Demi Moore",
+      "actor3": "Ashley Greene",
+      "actor4": "Douglas Booth",
+      "actor5": "Adam G. Sevani",
+      "critics_score": "17",
+      "director": "Liza Azuelos",
+      "genre": "Comedy",
+      "imdb_rating": "4.2",
+      "mpaa_rating": "PG-13",
+      "release_year": "2012",
+      "runtime": "97",
+      "title": "LOL",
+        
+      };
+
+      expect(movies.length).toEqual(651);
+      expect(movies[0]).toEqual(firstMovie);
+      expect(movies[650]).toEqual(lastMovie);
+
       done();
     });
   });
@@ -28,7 +67,6 @@ describe("The server's Post recmovies endpoint", () => {
       { json: requestData },
       (error:Error | null, response:request.Response | null, body:string) => {
         expect(response?.statusCode).toBe(200);
-        expect(Array.isArray(JSON.parse(body))).toBe(true);
         done();
       }
     );
@@ -39,8 +77,8 @@ describe("The server's Post recmovies endpoint", () => {
       emotion: "Happy",
       occasion: "Date Night",
       mpaa_rating: "PG-13",
-      start_release_year: 1990,
-      last_release_year: 1999
+      start_release_year: 1700,
+      last_release_year: 1710
     };
 
     request.post(
@@ -56,11 +94,11 @@ describe("The server's Post recmovies endpoint", () => {
 
   it("Returns status code 206 and a list of movies if none are found in the specified range, but some are found after expanding the range", (done) => {
     const requestData = {
-      emotion: "Happy",
-      occasion: "Date Night",
-      mpaa_rating: "R",
-      start_release_year: 2010,
-      last_release_year: 2015
+      emotion: "Neutral",
+      occasion: "Bored and Alone",
+      mpaa_rating: "G",
+      start_release_year: 2000,
+      last_release_year: 2001
     };
 
     request.post(
@@ -68,7 +106,6 @@ describe("The server's Post recmovies endpoint", () => {
       { json: requestData },
       (error:Error | null, response:request.Response | null, body:string) => {
         expect(response?.statusCode).toBe(206);
-        expect(Array.isArray(JSON.parse(body))).toBe(true);
         done();
       }
     );
